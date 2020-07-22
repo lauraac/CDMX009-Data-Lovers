a13ad04e9784efc41513d1b1de4b8db7ce4cb770
unknown <laura-a-c@hotmail.com>
2020-07-22 03:21:50 -0500

    filter

--- src/data.js
+++ src/data.js
@@ -1,13 +1,68 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
+export const filterId = (data) => {
+  let id= data.results.filter(result => result.id);
+   console.log(id)
+ }
 
-// esta es una función de ejemplo
+export const filterName = (data) => {
+  let name= data.results.filter(result => result.name);
+   console.log(name)
+ }
+ 
+ // STATUS
+ export const filterAlive=(data) =>{
+  let alive=data.results.filter(result=>result.status=="Alive")
+  console.log(alive)
+}
+export const filterDead=(data)=>{
+  let dead=data.results.filter(result=>result.status=="Dead")
+  console.log(dead)
+}
+export const filterUnknown1=(data)=>{
+  let unknown=data.results.filter(result=>result.status=="unknown")
+  console.log(unknown)
+}
 
-export const example = () => {
-  return 'example';
-};
+// SPECIES
+export const filterHuman = (data) => {
+  let human= data.results.filter(result => result.species==="Human");
+   console.log(human)
+ }
+ 
+ export const filterAlien =(data )=>{
+   let alien= data.results.filter(result =>result.species=="Alien")
+   console.log(alien)
+ }
+ 
+export const filterHumanoid=(data)=>{
+  let humanoid=data.results.filter(result=>result.species=="Humanoid")
+  console.log(humanoid)
+}
+  export const filterPoopybutthole=(data)=>{
+    let poopybutthole=data.results.filter(result=>result.species=="Poopybutthole")
+    console.log(poopybutthole)
+  }
+  
+  export const filterUnknown=(data) =>{
+    let unknown=data.results.filter(result=>result.species=="unknown")
+    console.log(unknown)
+  }
+
+  
+// GENDER
+  export const filterMale=(data)=>{
+    let gender=data.results.filter(result=>result.gender=="Male")
+    console.log(gender)
+  }
+  export const filterFemale=(data)=>{
+    let female=data.results.filter(result=>result.gender=="Female")
+    console.log(female)
+  }
+  export const filterUnknown2=(data)=>{
+    let unknown=data.results.filter(result=>result.gender=="unknown")
+    console.log(unknown)
+  }
+
+  // export const filterImage = (data) => {
+  //   let image= data.results.filter(result => result.image);
+  //    console.log(image)
+  //  }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { example } from './data.js';
-
-console.log(example);
-
+// import {example,anotherExample} from './data.js';
+import {filterId,filterName,filterAlive,filterDead} from './data.js';
+import {filterUnknown1,filterHuman,filterAlien,filterHumanoid} from './data.js';
+import {filterPoopybutthole,filterUnknown,filterMale,filterFemale,filterUnknown2} from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+// ejecutando la funcion
+filterId(data)
+filterName(data)
+filterAlive(data)
+filterDead(data)
+filterUnknown1(data)
+filterHuman(data)
+filterAlien(data)
+filterHumanoid(data)
+filterPoopybutthole(data)
+filterUnknown(data)
+filterMale(data)
+filterFemale(data)
+filterUnknown2(data)
